83e408faa19ec84956275c736bc46e72d5be3afe
Антон Андрущенко <aaandruschenko@edu.hse.ru>
2023-05-02 20:11:29 +0300

    sin_cos function

--- __main__.py
+++ __main__.py
@@ -5,5 +5,6 @@ from lr_scheduler import ConstLrScheduler
 
 if __name__ == "__main__":
     lr_scheduler = ConstLrScheduler(1e-4)
-    gradient_descent = GradientDescent(lr_scheduler, func.mean_quad, func.mean_quad_grad, num_args=2)
-    print(gradient_descent.descent())
+    gradient_descent = GradientDescent(lr_scheduler, func.sin_cos, func.sin_cos_grad, num_args=2, tol=1e-8)
+    min_point = gradient_descent.descent()
+    print(min_point, func.mean_quad(min_point))
--- func.py
+++ func.py
@@ -1,8 +1,31 @@
+from functools import lru_cache
 import numpy as np
+import math
+
+
+@lru_cache
+def sqrt_primes():
+    return np.sqrt(np.array([2] + list(filter(lambda k: (k % np.arange(3, 1 + int(math.sqrt(k)), 2)).all(), range(1, 10000 + 1, 2)))))
 
 
 def mean_quad(*args) -> float:
     return np.mean(np.square(np.array(args)))
 
+
 def mean_quad_grad(*args) -> np.ndarray:
     return np.array(args) * 2 / len(args)
+
+
+def sin_cos(*args) -> float:
+    arr = np.array(args)
+    coefs = sqrt_primes()[:2 * len(args)]
+    coefs_sin = coefs[:len(args)]
+    coefs_cos = coefs[len(args):]
+    return np.sum(np.sin(arr * coefs_sin) + np.cos(arr * coefs_cos))
+
+def sin_cos_grad(*args) -> float:
+    arr = np.array(args)
+    coefs = sqrt_primes()[:2 * len(args)]
+    coefs_sin = coefs[:len(args)]
+    coefs_cos = coefs[len(args):]
+    return np.sum(np.sin(arr * coefs_cos) - np.cos(arr * coefs_sin))
